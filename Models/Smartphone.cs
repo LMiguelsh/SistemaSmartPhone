beffc345b5af423952d105b531f146defca3aba9
LMiguelsh <110040313+LMiguelsh@users.noreply.github.com>
2022-09-23 11:36:37 -0300

    Add files via upload

---
+++ Models/Smartphone.cs
@@ -0,0 +1,32 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("99999", "nokia 10", "imei", 10000);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Jogo da cobrinha");
+
+Iphone iphone = new Iphone("888888", "iphone xr", "imei", 64000);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Fruit Ninja");
